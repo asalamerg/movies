27ef76970384a0480677f854de560de48da5039e
ahmad <asalameh02gmail.comm>
2025-04-30 10:56:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( Movies());
+}
+
+class Movies extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
